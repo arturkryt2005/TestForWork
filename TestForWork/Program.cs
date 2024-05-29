edf4b561a20743f137576904240efd791079d7c3
Артур <125348508+arturkryt2005@users.noreply.github.com>
2024-05-29 19:07:02 +0300

    Добавьте файлы проекта.

---
+++ TestForWork/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = 5; 
+
+        int[,] matrix = new int[N, N];
+
+        int summa = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                matrix[i, j] = i + j;
+                if(i == j)
+                    summa += matrix[i, j];
+            }
+        }
+
+        Console.WriteLine("Матрица, заполненная числами, равными сумме индексов:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                Console.Write(matrix[i, j] + " ");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine($"Сумма диагональных элементов = {summa}");
+    }
+}
